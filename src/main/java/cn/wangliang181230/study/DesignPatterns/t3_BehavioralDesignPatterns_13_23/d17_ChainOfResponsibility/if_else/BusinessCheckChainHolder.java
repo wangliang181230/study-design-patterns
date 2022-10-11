fbcc2ca789312c4e9fe1dd7a5d456f70fc792fef
王良 <841369634@qq.com>
2022-10-11 18:06:53 +0800

    优化 17.责任链模式 例子

--- src/main/java/cn/wangliang181230/study/DesignPatterns/t3_BehavioralDesignPatterns_13_23/d17_ChainOfResponsibility/if_else/BusinessCheckChainHolder.java
+++ src/main/java/cn/wangliang181230/study/DesignPatterns/t3_BehavioralDesignPatterns_13_23/d17_ChainOfResponsibility/if_else/BusinessCheckChainHolder.java
@@ -2,7 +2,6 @@ package cn.wangliang181230.study.DesignPatterns.t3_BehavioralDesignPatterns_13_2
 
 import java.util.List;
 
-import cn.wangliang181230.study.DesignPatterns.common.OrderUtils;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,21 +10,7 @@ class BusinessCheckChainHolder {
 	private final IBusinessCheckChain chain;
 
 	public BusinessCheckChainHolder(List<IBusinessCheckChain> chains) {
-		this.chain = this.join(chains);
-	}
-
-
-	private IBusinessCheckChain join(List<IBusinessCheckChain> chains) {
-		// 先排序
-		OrderUtils.sortByOrder(chains);
-
-		// 将前后连接起来
-		for (int i = 1; i < chains.size(); i++) {
-			chains.get(i - 1).setNext(chains.get(i));
-		}
-
-		// 返回第1个节点作为链的入口
-		return chains.get(0);
+		this.chain = BusinessCheckChainJointer.join(chains);
 	}
 
 	public IBusinessCheckChain get() {
